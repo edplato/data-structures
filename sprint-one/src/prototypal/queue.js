322821a434a617c5ee15f14636948b0e47c34a68
Edward Plato <edplatomail@gmail.com>
2017-08-23 21:50:03 -0700

    Pass tests for prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -10,15 +10,19 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
-
+  this.storage[this.first] = value;
+  this.first ++;
 };
 
 queueMethods.dequeue = function() {
-
+  if (this.first - this.last > 0) {
+    let dequeuer = this.storage[this.last];
+    delete this.storage[this.last];
+    this.last ++;
+    return dequeuer;
+  }
 };
 
 queueMethods.size = function() {
   return this.first - this.last;
 };
-
-
