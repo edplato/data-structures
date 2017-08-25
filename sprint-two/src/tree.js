8499c11bdd59b9b4342ce340445e8f932fe45667
Edward Plato <edplatomail@gmail.com>
2017-08-24 21:16:09 -0700

    Pass tests for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -13,13 +13,26 @@ var treeMethods = {};
 treeMethods.addChild = function(value) {
   let node = new Tree(value);
   this.children.push(node);
-  //console.log(node);
 };
 
 // A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
 treeMethods.contains = function(target) {
-};
 
+  var result = false;
+  var checkIt = function(tree, target) {
+    if (tree.value === target) {
+      result = true;
+    }
+
+    if(tree.children.length > 0) {
+      for(var i = 0; i < tree.children.length; i++) {
+        checkIt(tree.children[i], target);
+      }
+    }
+  };
+  checkIt(this, target);
+  return result;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
