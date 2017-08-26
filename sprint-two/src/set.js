6d4e72801fe03c5355cae3908883218309a6fdb2
Edward Plato <edplatomail@gmail.com>
2017-08-25 19:26:15 -0700

    Pass first tests for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  console.log(this);
+  this.storage.push(item);
+  console.log(this.storage);
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.storage.contains(item)) {
+    for (var i = 0; i < this.storage.length; i++) {
+      this.storage.splice(i, 1);
+    }
+  }
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
