89cda4028c28852d28de473929d8d8e96a7717ea
Edward Plato <edplatomail@gmail.com>
2017-08-26 10:38:31 -0700

    Add new test for BST

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -1,8 +1,10 @@
 describe('binarySearchTree', function() {
   var binarySearchTree;
+  var highValueBST;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    highValueBST = new BinarySearchTree(15);
+    binarySearchTree = new BinarySearchTree(5);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -18,6 +20,23 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(6);
     expect(binarySearchTree.left.right.value).to.equal(3);
     expect(binarySearchTree.right.left.value).to.equal(6);
+  });
+
+  it('should insert a massive of values at the correct location in the tree', function() {
+    highValueBST.insert(2);
+    highValueBST.insert(3);
+    highValueBST.insert(7);
+    highValueBST.insert(6);
+    highValueBST.insert(1);
+    highValueBST.insert(4);
+    highValueBST.insert(23);
+    highValueBST.insert(16);
+    highValueBST.insert(12);
+    highValueBST.insert(31);
+    highValueBST.insert(5);
+    highValueBST.insert(63);
+    expect(highValueBST.left.right.value).to.equal(3);
+    expect(highValueBST.left.right.right.right.value).to.equal(12);
   });
 
   it('should have a working "contains" method', function() {
